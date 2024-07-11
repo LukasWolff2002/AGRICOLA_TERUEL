ad9385bffb4673a08763bf5fad17bfa78916c938
LukasWolff2002 <lwolff@miuandes.cl>
2024-07-11 15:55:53 -0500

    avance

--- app/controllers/catalogo_controller.rb
+++ app/controllers/catalogo_controller.rb
@@ -1,5 +1,14 @@
 class CatalogoController < ApplicationController
 
+    def index
+        @catalogo_logo = Image.find_by(name: 'CATALOGO_LOGO')
+        @variedad_1 = Image.find_by(name: 'VARIEDAD_1')
+        @variedad_2 = Image.find_by(name: 'VARIEDAD_2')
+        @variedad_3 = Image.find_by(name: 'VARIEDAD_3')
+        @variedad_4 = Image.find_by(name: 'VARIEDAD_4')
+        @variedad_5 = Image.find_by(name: 'VARIEDAD_5')
+    end
+
     def tomate_granel
         @catalogo = Image.find_by(name: 'CATALOGO')
         @variedad_1 = Image.find_by(name: 'VARIEDAD_1')
