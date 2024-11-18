42d32ab4063783f33e3b77948993a7fdc2fc531d
LukasWolff2002 <lwolff@miuandes.cl>
2024-11-18 15:58:17 -0300

    avance

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,11 +10,14 @@ class CommentsController < ApplicationController
   
     def create
       @comment = Comment.new(comment_params)
+      Rails.logger.info "Intentando guardar el comentario"
       if @comment.save
-        # Enviar correo
+        Rails.logger.info "Comentario guardado exitosamente, enviando correo..."
         CommentMailer.new_comment_notification(@comment).deliver_now
-        redirect_to root_path, notice: 'Comment was successfully created.'
+        Rails.logger.info "Correo enviado"
+        redirect_to root_path, notice: 'Comentario enviado exitosamente.'
       else
+        Rails.logger.error "Error al guardar el comentario: #{@comment.errors.full_messages.join(", ")}"
         render :new, status: :unprocessable_entity
       end
     end
